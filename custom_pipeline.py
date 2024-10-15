2afd572aeb1cb8137a4211e262b35c75a553c2a1
yu <1830418154@qq.com>
2024-10-15 22:25:16 +0800

    Init

---
+++ custom_pipeline.py
@@ -0,0 +1,181 @@
+import torch
+import numpy as np
+
+from tqdm import tqdm
+from diffusers.schedulers import PNDMScheduler
+from base_pipeline import BasePipeline
+
+
+def cfg_aggregator(noise_pred_con, noise_pred_uncon):
+    return noise_pred_con - noise_pred_uncon
+
+
+def get_perpendicular_component(x, y):
+    """Get the component of x that is perpendicular to y"""
+    assert x.shape == y.shape
+    return x - ((torch.mul(x, y).sum()) / (torch.norm(y) ** 2)) * y
+
+
+def add_aggregator_v1(delta_noise_pred_src, w_src, delta_noise_pred_tgt, w_tgt):
+    return w_src * delta_noise_pred_src + w_tgt * get_perpendicular_component(
+        delta_noise_pred_tgt, delta_noise_pred_src
+    )
+
+
+def add_aggregator_v2(delta_noise_pred_src, w_src, delta_noise_pred_tgt, w_tgt):
+    return w_src * delta_noise_pred_src + w_tgt * (
+        get_perpendicular_component(delta_noise_pred_tgt, delta_noise_pred_src)
+        - delta_noise_pred_src
+    )
+
+
+def guidance_weight(t, w0, guidance_type: str, t_total=1000, clamp=4):
+    if guidance_type == "static":
+        w = w0
+    elif guidance_type == "linear":
+        w = w0 * 2 * (1 - t / t_total)
+    elif guidance_type == "cosine":
+        w = w0 * (np.cos(np.pi * t / t_total) + 1)
+    else:
+        raise ValueError("Unrecognized guidance type")
+    return max(clamp, w) if clamp else w
+
+
+class CustomPipeline(BasePipeline):
+
+    @torch.no_grad()
+    def __call__(
+        self,
+        prompt,
+        batch_size=1,
+        height=512,
+        width=512,
+        num_inference_steps=50,
+        guidance_scale=7.5,
+        eta=0.0,
+        latents=None,
+        unconditioning=None,
+        neg_prompt=None,
+        ref_intermediate_latents=None,
+        return_intermediates=False,
+        w_src=1.0,
+        w_tgt=1.0,
+        use_plain_cfg=False,
+        guidance_type: str = "static",
+        **kwds,
+    ):
+        DEVICE = (
+            torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+        )
+        if isinstance(prompt, list):
+            batch_size = len(prompt)
+        elif isinstance(prompt, str):
+            if batch_size > 1:
+                prompt = [prompt] * batch_size
+
+        # text embeddings
+        text_input = self.tokenizer(
+            prompt, padding="max_length", max_length=77, return_tensors="pt"
+        )
+
+        text_embeddings = self.text_encoder(text_input.input_ids.to(DEVICE))[0]
+        print("input text embeddings :", text_embeddings.shape)
+        if kwds.get("dir"):
+            dir = text_embeddings[-2] - text_embeddings[-1]
+            u, s, v = torch.pca_lowrank(dir.transpose(-1, -2), q=1, center=True)
+            text_embeddings[-1] = text_embeddings[-1] + kwds.get("dir") * v
+            print(u.shape)
+            print(v.shape)
+
+        # define initial latents
+        latents_shape = (batch_size, self.unet.in_channels, height // 8, width // 8)
+        if latents is None:
+            latents = torch.randn(latents_shape, device=DEVICE)
+        else:
+            assert (
+                latents.shape == latents_shape
+            ), f"The shape of input latent tensor {latents.shape} should equal to predefined one."
+
+        # unconditional embedding for classifier free guidance
+        if guidance_scale > 1.0:
+            max_length = text_input.input_ids.shape[-1]
+            if neg_prompt:
+                uc_text = neg_prompt
+            else:
+                uc_text = ""
+            # uc_text = "ugly, tiling, poorly drawn hands, poorly drawn feet, body out of frame, cut off, low contrast, underexposed, distorted face"
+            unconditional_input = self.tokenizer(
+                [uc_text] * batch_size,
+                padding="max_length",
+                max_length=77,
+                return_tensors="pt",
+            )
+            # unconditional_input.input_ids = unconditional_input.input_ids[:, 1:]
+            unconditional_embeddings = self.text_encoder(
+                unconditional_input.input_ids.to(DEVICE)
+            )[0]
+            text_embeddings = torch.cat(  # (2B, 77, 1024)
+                [unconditional_embeddings, text_embeddings], dim=0
+            )
+
+        print("latents shape: ", latents.shape)  # (B, 4, 64, 64)
+        # iterative sampling
+        self.scheduler.set_timesteps(num_inference_steps)
+        # print("Valid timesteps: ", reversed(self.scheduler.timesteps))
+        latents_list = [latents]
+        for i, t in enumerate(tqdm(self.scheduler.timesteps, desc="DDIM Sampler")):
+            if ref_intermediate_latents is not None:
+                # note that the batch_size >= 2
+                latents_ref = ref_intermediate_latents[-1 - i]
+                _, latents_cur = latents.chunk(2)
+                latents = torch.cat([latents_ref, latents_cur])
+
+            if guidance_scale > 1.0:
+                model_inputs = torch.cat([latents] * 2)
+            else:
+                model_inputs = latents
+            if unconditioning is not None and isinstance(unconditioning, list):
+                _, text_embeddings = text_embeddings.chunk(2)
+                text_embeddings = torch.cat(
+                    [unconditioning[i].expand(*text_embeddings.shape), text_embeddings]
+                )
+            # predict noise
+            noise_pred = self.unet(
+                model_inputs, t, encoder_hidden_states=text_embeddings
+            ).sample
+
+            # aggregate noise
+            if guidance_scale > 1.0:
+                noise_pred_uncon, noise_pred_con = noise_pred.chunk(2, dim=0)
+
+                if use_plain_cfg:
+                    noise_pred = noise_pred_uncon + guidance_scale * cfg_aggregator(
+                        noise_pred_con, noise_pred_uncon
+                    )  # (B, 4, 64, 64)
+                else:
+                    assert noise_pred_con.shape[0] == noise_pred_uncon.shape[0] == 2
+                    delta_noise_pred_src = noise_pred_con[0] - noise_pred_uncon[0]
+                    delta_noise_pred_tgt = noise_pred_con[1] - noise_pred_uncon[1]
+
+                    noise_pred = noise_pred_uncon + guidance_weight(
+                        t, guidance_scale, guidance_type
+                    ) * add_aggregator_v1(
+                        delta_noise_pred_src, w_src, delta_noise_pred_tgt, w_tgt
+                    )
+
+            # compute the previous noise sample x_t -> x_t-1
+            if isinstance(self.scheduler, PNDMScheduler):
+                latents = self.scheduler.step(noise_pred, t, latents).prev_sample
+            else:
+                latents, _ = self.step(noise_pred, t, latents)
+            latents_list.append(latents)
+
+        image = self.latent2image(latents, return_type="pt")
+        if return_intermediates:
+            latents_list = [
+                self.latent2image(img, return_type="pt") for img in latents_list
+            ]
+            return image, latents_list
+
+        # if noises have been aggregated then they are the same
+        return image[0] if torch.all(image[0] == image[1]) else image
---
+++ test.py
@@ -0,0 +1,64 @@
+import os
+import torch
+from torchvision.utils import save_image
+from pytorch_lightning import seed_everything
+
+
+from custom_pipeline import CustomPipeline
+from attention_utils import register_attention_editor_diffusers, AttentionBase
+
+src_start, src_inc, src_n = 1.3, 0.1, 2
+tgt_start, tgt_inc, tgt_n = 1.0, 0.1, 21
+prompts = [
+    "Catwoman holding a sniper rifle",
+    "Catwoman holding a sniper rifle and wearing a hat",
+]
+
+# set seed
+seed = 0
+seed_everything(seed)
+
+# set device
+torch.cuda.set_device(1)
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+# set output path
+out_dir = "./exp/add/"
+os.makedirs(out_dir, exist_ok=True)
+sample_count = len(os.listdir(out_dir))
+out_dir = os.path.join(out_dir, f"sample_{sample_count}")
+os.makedirs(out_dir, exist_ok=True)
+
+# initialize model
+model_path = "/mnt/hdd1/hongyu/models/stable-diffusion-2-1-base"
+model = CustomPipeline.from_pretrained(model_path).to(device)
+
+# initialize the noise map
+start_code = torch.randn([1, 4, 64, 64], device=device)
+start_code = start_code.expand(len(prompts), -1, -1, -1)
+
+# use original attention
+editor = AttentionBase()
+register_attention_editor_diffusers(model, editor)
+
+# inference the synthesized image
+src_weights = [round(src_start + src_inc * i, 2) for i in range(src_n)]
+tgt_weights = [round(tgt_start + tgt_inc * i, 2) for i in range(tgt_n)]
+
+for w_src in src_weights:
+    for w_tgt in tgt_weights:
+        image = model(
+            prompts,
+            latents=start_code,
+            guidance_scale=7.5,
+            w_src=w_src,
+            w_tgt=w_tgt,
+            guidance_type="static",
+        )
+
+        # save the synthesized image
+        save_image(
+            image,
+            os.path.join(out_dir, f"{w_src}_{w_tgt}.png"),
+        )
+        print("Syntheiszed images are saved in", out_dir)
