547be806acc816af163bb82a75888988ec266ba4
yu <1830418154@qq.com>
2024-11-01 15:06:29 +0800

    Add MasaCtrl for CLIP score

--- metrics/clip_score.py
+++ metrics/clip_score.py
@@ -4,15 +4,20 @@ from torchvision.io import read_image
 from metrics.clip_utils import calculate_clip_score
 
 
+# specify the model to test
+config_file = 'metrics/images.yaml'
+model = 'masactrl'
+mode = 'add'
+
 # set device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # load images and prompts
-with open("metrics/images.yaml") as f:
+with open(config_file) as f:
     dataset = yaml.safe_load(f)
 
-images = [read_image(img) for img in dataset['tgt_images']]
-prompts = dataset['tgt_prompts']
+images = [read_image(data[model]['tgt_image']) for data in dataset[mode].values()]
+prompts = [data['tgt_prompt'] for data in dataset[mode].values()]
 
 clip_score = calculate_clip_score(images, prompts)
-print(f"CLIP score: {clip_score}")  # SD: 14.739
+print(f"CLIP score: {clip_score}")  # SD: 35.3821
