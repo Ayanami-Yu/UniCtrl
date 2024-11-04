bff8676dcaaac051884cbaecfbc079cdc35aa947
yu <1830418154@qq.com>
2024-11-04 14:48:35 +0800

    Calculate CLIP for video addition

--- metrics/clip_score.py
+++ metrics/clip_score.py
@@ -1,15 +1,19 @@
 import torch
 import yaml
+import os
 from torchvision.io import read_image
 from metrics.clip_utils import calculate_clip_score
 
 
 # specify the model to test
-# available models: sd, masactrl, p2p
+# available config files: metrics/images.yaml, metrics/videos.yaml
+# available modalities: image, video
+# available models: sd, masactrl, p2p, animatediff, fatezero
 # available modes: add, rm
-config_file = "metrics/images.yaml"
-model = "sd"
-mode = "rm"
+config_file = "metrics/videos.yaml"
+modality = "video"
+model = "fatezero"
+mode = "add"
 
 # set device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -18,10 +22,23 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 with open(config_file) as f:
     dataset = yaml.safe_load(f)
 
-images = [read_image(data[model]["tgt_image"]) for data in dataset[mode].values()]
-prompts = [data["tgt_prompt"] for data in dataset[mode].values()]
+if modality == "image":
+    images = [read_image(data[model]["tgt_image"]) for data in dataset[mode].values()]
+    prompts = [data["tgt_prompt"] for data in dataset[mode].values()]
+elif modality == "video":
+    # NOTE We haven't measured temporal consistency as it's not strongly
+    # related to the scope of our research.
+    images = []
+    prompts = []
+    for data in dataset[mode].values():
+        path = data[model]["tgt_images"]
+        images.extend([read_image(os.path.join(path, img)) for img in os.listdir(path)])
+        prompts.extend([data["tgt_prompt"]] * len(os.listdir(path)))
+else:
+    raise ValueError("Unrecognized modality")
 
-# Add: SD = 35.3821, MasaCtrl = 31.809, P2P = 34.6683
+# Add (image): SD = 35.3821, MasaCtrl = 31.809, P2P = 34.6683
+# Add (video): AnimateDiff = 36.3802, FateZero = 33.8866
 # Remove: SD = 28.6935
 clip_score = calculate_clip_score(images, prompts)
 print(f"CLIP score: {clip_score}")
--- metrics/directional_clip.py
+++ metrics/directional_clip.py
@@ -1,5 +1,6 @@
 import torch
 import yaml
+import os
 import numpy as np
 
 from torchvision.io import read_image
@@ -13,11 +14,14 @@ from metrics.clip_utils import DirectionalSimilarity
 
 
 # specify the model to test
-# available models: sd, masactrl, p2p
+# available config files: metrics/images.yaml, metrics/videos.yaml
+# available modalities: image, video
+# available models: sd, masactrl, p2p, animatediff, fatezero
 # available modes: add, rm
-config_file = "metrics/images.yaml"
-model = "sd"
-mode = "rm"
+config_file = "metrics/videos.yaml"
+modality = "video"
+model = "fatezero"
+mode = "add"
 
 # set device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -33,10 +37,26 @@ image_encoder = CLIPVisionModelWithProjection.from_pretrained(clip_id).to(device
 with open(config_file) as f:
     dataset = yaml.safe_load(f)
 
-src_images = [read_image(data[model]["src_image"]) for data in dataset[mode].values()]
-tgt_images = [read_image(data[model]["tgt_image"]) for data in dataset[mode].values()]
-src_prompts = [data["src_prompt"] for data in dataset[mode].values()]
-tgt_prompts = [data["tgt_prompt"] for data in dataset[mode].values()]
+if modality == "image":
+    src_images = [read_image(data[model]["src_image"]) for data in dataset[mode].values()]
+    tgt_images = [read_image(data[model]["tgt_image"]) for data in dataset[mode].values()]
+    src_prompts = [data["src_prompt"] for data in dataset[mode].values()]
+    tgt_prompts = [data["tgt_prompt"] for data in dataset[mode].values()]
+elif modality == "video":
+    src_images = []
+    tgt_images = []
+    src_prompts = []
+    tgt_prompts = []
+    for data in dataset[mode].values():
+        src_path = data[model]["src_images"]
+        tgt_path = data[model]["tgt_images"]
+
+        src_images.extend([read_image(os.path.join(src_path, img)) for img in os.listdir(src_path)])
+        tgt_images.extend([read_image(os.path.join(tgt_path, img)) for img in os.listdir(tgt_path)])
+        src_prompts.extend([data["src_prompt"]] * len(os.listdir(src_path)))
+        tgt_prompts.extend([data["tgt_prompt"]] * len(os.listdir(tgt_path)))
+else:
+    raise ValueError("Unrecognized modality")
 
 # calculate directional CLIP similarity
 dir_similarity = DirectionalSimilarity(
@@ -55,6 +75,7 @@ for i in range(len(src_images)):
     )
     scores.append(float(similarity_score.detach().cpu()))
 
-# Add: SD = 0.2147, MasaCtrl = 0.0785, P2P = 0.162
+# Add (image): SD = 0.2147, MasaCtrl = 0.0785, P2P = 0.162
+# Add (video): AnimateDiff = 0.2297, FateZero = 0.031
 # Remove: SD = 0.1414
 print(f"CLIP directional similarity: {round(np.mean(scores), 4)}")
