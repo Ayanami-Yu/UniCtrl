ef131b644a0ba5b48e0008e74f4613a7d98ac671
yu <1830418154@qq.com>
2025-02-21 23:17:53 +0800

    Add VidToMe metrics

--- metrics/directional_clip.py
+++ metrics/directional_clip.py
@@ -17,10 +17,10 @@ from metrics.clip_utils import DirectionalSimilarity
 # available config files: metrics/images.yaml, metrics/videos.yaml
 # available modalities: image, video
 # available image models: sd, masactrl, p2p, sega, ledits_pp, mdp, cg
-# available video models: animatediff, fatezero, tokenflow
+# available video models: animatediff, fatezero, tokenflow, vidtome
 # available modes: add, rm
 modality = "video"
-model = "tokenflow"
+model = "vidtome"
 mode = "rm"
 config_file = "metrics/images.yaml" if modality == "image" else "metrics/videos.yaml"
 
@@ -92,8 +92,8 @@ for i in range(len(src_images)):
     scores.append(float(similarity_score.detach().cpu()))
 
 # Add (image): SD = 0.2388, MasaCtrl = 0.0956, P2P = 0.1081, SEGA = 0.1369, LEDITS++ = 0.2109, MDP = 0.1836, CG = 0.2255
-# Add (video): AnimateDiff = 0.2297, FateZero = 0.0652, TokenFlow = 0.0986
+# Add (video): AnimateDiff = 0.2297, FateZero = 0.0652, TokenFlow = 0.0986, VidToMe = 0.0893
 
 # Remove (image): SD = 0.1243, MasaCtrl = 0.0339, P2P = 0.072, SEGA = 0.0944, LEDITS++ = 0.1641, MDP = 0.1205, CG = 0.1274
-# Remove (video): AnimateDiff = 0.1735, FateZero = 0.0241, TokenFlow = 0.1176
+# Remove (video): AnimateDiff = 0.1735, FateZero = 0.0241, TokenFlow = 0.1176, VidToMe = 0.1219
 print(f"CLIP directional similarity: {round(np.mean(scores), 4)}")
