2825c51c8c81dba02a8dd0833809d3128507bcd0
yu <1830418154@qq.com>
2025-03-03 16:27:57 +0800

    Add PIE image results

--- res2yaml.py
+++ res2yaml.py
@@ -17,7 +17,8 @@ content = {
 
 # load the previous yaml file if provided
 if prev_file:
-    data_prev = yaml.safe_load(prev_file)
+    with open(prev_file, 'r') as f:
+        data_prev = yaml.safe_load(f)
     for mode in data_prev.keys():
         content[mode] = {k: v for k, v in data_prev[mode].items()}
 
@@ -48,23 +49,23 @@ for img_name in os.listdir(res_path):
     # save the images to target directory
     mid_dirs = ["default", "sd"]
     img_save_paths = [
-        f"metrics/images/{fields[i]}/{mid_dirs[i]}/{conf.ctrl_mode}/{img_name}_{extracted[i]}.png"
+        f"metrics/images/{fields[i]}/{mid_dirs[i]}/{conf['ctrl_mode']}/{img_name}_{extracted[i]}.png"
         for i in range(2)
     ]
     for i in range(2):
         imgs[i].save(img_save_paths[i])
 
     # add a new case into content
-    content[conf.ctrl_mode][img_name] = {
-        "seed": conf.seed,
-        "src_prompt": conf.prompts[0],
+    content[conf['ctrl_mode']][img_name] = {
+        "seed": conf['seed'],
+        "src_prompt": conf['prompts'][0],
         "tgt_prompt": (
             {
                 "default": "",
-                "sd": conf.prompts[1],
+                "sd": conf['prompts'][1],
             }
-            if conf.ctrl_mode == "rm"
-            else conf.prompts[1]
+            if conf['ctrl_mode'] == "rm"
+            else conf['prompts'][1]
         ),
         "src_image": {
             "default": img_save_paths[0],
